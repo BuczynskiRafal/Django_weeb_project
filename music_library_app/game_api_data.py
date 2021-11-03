a1ab986b5e750f291e37dc19b734942e80fcd9e7
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-11-03 14:07:22 +0100

    Game library data scraping

---
+++ music_library_app/game_api_data.py
@@ -0,0 +1,16 @@
+import requests
+
+
+API_URL = "https://opentdb.com/api.php"
+parameters = {
+    "amount":10,
+    "type":"boolean"
+    }
+
+
+response = requests.get(API_URL, params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
+
+print(question_data)
